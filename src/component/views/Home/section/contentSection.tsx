dd3a0c9c7ca450aefb0cdf0e126fe706a6dee9c4
eli <eli@MacPCs-MacBook-Pro.local>
2022-09-14 14:39:17 +0430

    feat: work on home page and create baseUI

--- src/component/views/Home/section/contentSection.tsx
+++ src/component/views/Home/section/contentSection.tsx
@@ -1,47 +1,39 @@
 import { useState, useEffect } from 'react';
 import TextTransition, { presets } from "react-text-transition";
-import { Container, Header } from '../styles/contentSectionStyles';
+import { Container, Header, TextWrapper ,LinkButton} from '../styles/contentSectionStyles';
 import { data } from '../content/data';
+import { P } from "../../../utilities/baseUI";
 
 const ContentSection = () => {
   const [index, setIndex] = useState(0);
   const { header, subHeader, career, career2, mainContent, btn } = data;
-  // const renderCareer = () => career.map((value: { title: string }) => (
-  //   <h2 key={value.title}>{value.title}</h2>));
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setIndex(index => index + 1);
-       if (index > career2.length) setIndex(0);
+      if (index > career2.length) setIndex(0);
     },
       5000 // every 3 seconds
     );
     return () => clearTimeout(intervalId);
   }, [career2.length, index]);
-  return (<Container>
-    <Header>{header}</Header>
-    <Header>{subHeader}</Header>
-    {/* {renderCareer()} */}
- {/* <h1>
-      <TextTransition
-        text={career2[index % career2.length]}
-        springConfig={presets.wobbly}
-      />
-
-    </h1> */}
-        <h1>  {`${career2[index % career2.length]}`.split("").map((n, i) => (
-      <TextTransition
-        key={i}
-        text={n}
-        delay={i * 100}
-        inline
-        noOverflow={true}
-        springConfig={presets.wobbly}
-      />
-    ))}
-    </h1>
-    <p>{mainContent}</p>
-    <a href={btn.src} download="my-awesome-resume">{btn.name}</a>
-  </Container>)
+  return (
+    <Container>
+      <Header>{header}</Header>
+      <Header>{subHeader}</Header>
+      <TextWrapper>  {`${career2[index % career2.length]}`.split("").map((n, i) => (
+        <TextTransition
+          key={i}
+          text={n}
+          delay={i * 100}
+          inline
+          noOverflow={true}
+          springConfig={presets.wobbly}
+        />
+      ))}
+      </TextWrapper>
+      <P>{mainContent}</P>
+      <LinkButton href={btn.src} download="my-awesome-resume">{btn.name}</LinkButton>
+    </Container>)
 };
 export default ContentSection;
--- src/component/views/Home/styles/photoSectionStyles.ts
+++ src/component/views/Home/styles/photoSectionStyles.ts
@@ -1,14 +1,42 @@
 import styled from "styled-components";
+import { media } from "../../../utilities/grid";
 
 export const Container = styled.div`
-  background-color: white;
-  border-top: 1rem solid ${props => props.theme.color.green};
-  border-right: 1rem solid ${props => props.theme.color.green};
-  width: 20%;   
   display: flex;
-  padding: 1rem;
+  ${media.from.tablet`
+   max-width: 33%;
+   justify-content: center;
+  `}   
 `;
 
-export const Media = styled.img`
+export const WrapperMedia = styled.div`
+  position: relative;
+  diplay: flex;
+  justify-content: center;
 
+  ${({ theme }) => media.from.tablet`
+  border: none;
+  box-shadow: 4px 4px 15px 0px rgb(0 0 0 / 10%);
+  :before{
+    position: absolute;
+    content: "";
+    top: -30px;
+    right: -30px;
+    display: inline-block;
+    border: 1rem solid ${theme.color.green};
+    z-index: -1;
+    width: 100%;
+    height: 95%;
+  }
+ `}   
+`;
+
+export const Media = styled.img` 
+  width: 90%;
+  border: 1rem solid ${props=>props.theme.color.green};
+
+  ${media.from.tablet`
+    width: 100%;
+    border: none;
+  `};
 `;
