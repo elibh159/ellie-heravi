1361cc28e19618b92964673e1532833712cc7c01
eli <eli@MacPCs-MacBook-Pro.local>
2022-10-08 09:22:58 +0330

    feat/implement about me page

--- src/component/views/AboutMe/section/contentSection.tsx
+++ src/component/views/AboutMe/section/contentSection.tsx
@@ -1,11 +1,28 @@
+
 import { P } from "../../../utilities/baseUI";
-import { Container } from '../styles/contentSectionStyles';
+import { Container, TextWrapper, Title, ItemList, ListBox, ItemListTitle, LinkButton ,ButtonBox} from '../styles/contentSectionStyles';
+import { data } from '../content/data';
 
 const ContentSection = () => {
 
-  return (
-    <Container>
-      <P>About me</P>
-    </Container>)
+    return (
+        <Container>
+            <Title>{data.subHeader}</Title>
+            <TextWrapper>{data.header}</TextWrapper>
+            <P>{data.mainContent}</P>
+            <ListBox>
+                {data.listOfInfo.map(({ name, value, id, type }) =>
+                (<ItemList key={id}>
+                    <ItemListTitle>{name}:</ItemListTitle>
+                    {value}
+                </ItemList>
+                ))}
+            </ListBox>
+            <ButtonBox>
+                {data.btns.map(({ src, name }) => (
+                    <LinkButton to={src}>{name}</LinkButton>
+                ))}
+            </ButtonBox>
+        </Container>)
 };
 export default ContentSection;
